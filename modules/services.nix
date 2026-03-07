176e75c10012b6f550641d2dcc82bc64f3cdbc81
Sigileth <ankalagon@ryuma.org>
2026-03-07 08:00:29 +0100

    add spice-webdavd to services

--- modules/services.nix
+++ modules/services.nix
@@ -5,5 +5,5 @@
   services.avahi.enable = true;
   services.avahi.nssmdns4 = true;
   services.flatpak.enable = true;
-  services.spice-webdav.enable = true;
+  services.spice-webdavd.enable = true;
 }
