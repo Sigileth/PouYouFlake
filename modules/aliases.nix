1093777c8085ec0ed956c3586c484a04f181180d
Sigileth <ankalagon@ryuma.org>
2026-03-14 21:46:12 +0100

    update aliases.nix

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -1,7 +1,7 @@
 { config, pkgs, pkgs-unstable, lib, ...}:
 {
   environment.shellAliases = {
-        pouyou-update = "sudo nix flake update --flake /home/ank/Flake && nh os boot /home/ank/flake -H PouYou -- --accept-flake-config";
+        pouyou-update = "sudo nix flake update --flake /home/ank/Flake && nh os boot /home/ank/Flake -H PouYou -- --accept-flake-config";
         pouyou-build = "nh os build /home/ank/Flake -H PouYou -- --accept-flake-config";
         pouyou-switch = "sudo nix flake update --flake /home/ank/Flake && nh os switch /home/ank/Flake -H PouYou -- --accept-flake-config";
         pouyou-history = "nix store diff-closures $(ls -d1v /nix/var/nix/profiles/system-*-link | tail -n 2)";
