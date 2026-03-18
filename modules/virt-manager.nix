13b4b6998d72909593ace8494ce350063ea4eadb
Sigileth <ankalagon@ryuma.org>
2026-03-18 17:33:24 +0000

    Update virt-manager.nix

--- modules/virt-manager.nix
+++ modules/virt-manager.nix
@@ -1,1 +1,7 @@
-
+{ config, pkgs, pkgs-unstable, lib, ...}:
+{
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = [ "ank" ];
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+}
