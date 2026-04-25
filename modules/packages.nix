2da5ae1eb2faa1c2f39c1f78eae1e15f85823971
Sigileth <ankalagon@ryuma.org>
2026-04-25 17:33:48 +0000

    Update packages.nix

--- modules/packages.nix
+++ modules/packages.nix
@@ -2,26 +2,64 @@
 
 {
   environment.systemPackages = [
-  pkgs.delfin
-  pkgs.fastfetch
-  pkgs.git
-  pkgs.handbrake
+  # Gaming
+  pkgs.faugus-launcher
   pkgs.heroic
-  pkgs.kdePackages.kate
   pkgs.kdePackages.kmahjongg
-  pkgs.libreoffice-qt-fresh
-  pkgs.librewolf
+  pkgs.kdePackages.kmines
   pkgs.mangohud
   pkgs.mangojuice
-  pkgs.nh
-  pkgs.popsicle
   pkgs.protonplus
   pkgs.protontricks
-  pkgs.spice-vdagent
+
+  # Multimedia
+  pkgs.delfin
+  pkgs.handbrake
   pkgs.strawberry
   pkgs.vlc
+
+  # Utils
+  pkgs.fastfetch
+  pkgs.git
+  pkgs.kdePackages.kate
+  pkgs.kronometer
+  pkgs.nh
+  pkgs.popsicle
+
+  # Compression
+  pkgs.arj
+  pkgs.brotli
+  pkgs.bzip2
+  pkgs.cpio
+  pkgs.gnutar
+  pkgs.gzip
+  pkgs.libarchive
+  pkgs.lrzip
+  pkgs.lz4
+  pkgs.lzop
+  pkgs.p7zip
+  pkgs.pbzip2
+  pkgs.pigz
+  pkgs.pixz
+  pkgs.unrar
+  pkgs.unzip
+  pkgs.xz
+  pkgs.zip
+  pkgs.zstd
+
+  # Office
+  pkgs.libreoffice-qt-fresh
+
+  # Internet
+  pkgs.librewolf
+  pkgs.vesktop
+  pkgs.vivaldi
+  
+  # Drivers
+  pkgs.spice-vdagent
   pkgs-unstable.mesa
   pkgs-unstable.pkgsi686Linux.mesa
+  
   ];
 
 }
