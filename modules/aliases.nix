a440ae80d4f3b47f69935b4366e288996df954de
Sigileth <ankalagon@ryuma.org>
2026-06-11 08:39:38 +0200

    add alias

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -5,5 +5,6 @@
         pouyou-build = "nh os build /home/ank/git/PouYouFlake -H PouYou -- --accept-flake-config";
         pouyou-switch = "sudo nix flake update --flake /home/ank/git/PouYouFlake && nh os switch /home/ank/git/PouYouFlake -H PouYou -- --accept-flake-config";
         pouyou-history = "nix store diff-closures $(ls -d1v /nix/var/nix/profiles/system-*-link | tail -n 2)";
+        pouyou-clean = "sudo nix-collect-garbage --delete-older-than 5d && sudo nixos-rebuild boot --flake /etc/nixos#GLF-OS";
   };
 }
